c761842a965851feeefda0a49d962d618b9dd830
Justine Gran <justine57.gran@gmail.com>
2026-01-16 12:59:55 +0000

    changement dans le code app.py

--- python-producer/app.py
+++ python-producer/app.py
@@ -2,21 +2,22 @@ from kafka import KafkaProducer
 import json
 
 # Define the Kafka broker and topic
-broker = '{RPI_IP}:9092'
+broker = 'my-kafka.gran4u-dev.svc.cluster.local:9092'
 topic = 'my-first-topic'
 
 # Create a Kafka producer
-#producer = KafkaProducer(
-#    bootstrap_servers=[broker],
-#   sasl_mechanism='SCRAM-SHA-256',
-#     security_protocol='SASL_PLAINTEXT',
-#     sasl_plain_username='user1',
-#     sasl_plain_password='FHp2sfyGox'
-#)
 producer = KafkaProducer(
     bootstrap_servers=[broker],
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+   sasl_mechanism='SCRAM-SHA-256',
+     security_protocol='SASL_PLAINTEXT',
+     sasl_plain_username='user1',
+     sasl_plain_password='B9R3L1I1Uy',
+     value_serializer=lambda v: json.dumps(v).encode('utf-8')
 )
+#producer = KafkaProducer(
+#    bootstrap_servers=[broker],
+#    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+ #)
 # Define the message to send
 message = {
     'key': 'this-is-a-key',
